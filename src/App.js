73537ce356cc5321358defc4985e81bdfb6ae88f
Jason Stathopulos <jstathopulos@gmail.com>
2020-04-17 10:50:53 -0400

    Add Login Button to Home Component
    Add the Login Button to Home component so that it can perform the login
    when the user wants to do a login.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Fragment } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, useHistory } from 'react-router-dom'
 
 import Home from './Home'
 import Profile from './Profile'
 import Nav from './Nav'
 
+import Auth from './auth/Auth'
+
 function App() {
+  const history = useHistory()
+  const auth = new Auth(history)
+
   return (
     <Fragment>
       <Nav />
       <main className="body">
         <Switch>
-          <Route path="/" exact component={Home} />
+          <Route path="/" exact render={() => <Home auth={auth} />} />
           <Route path="/profile" component={Profile} />
         </Switch>
       </main>
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Home = ({ auth }) => {
   return (
     <section>
       <h1>Home</h1>
+
+      <button onClick={auth.login}>Log In</button>
     </section>
   )
 }
