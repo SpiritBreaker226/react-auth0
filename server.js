8ed61151d7645506b7e7c11875aaad85a9d034ee
Jason Stathopulos <jstathopulos@gmail.com>
2020-04-17 18:13:56 -0400

    Add `checkJwt` to Confirm JWT is Valid
    Add `checkJwt` to confirm JWT is valid and to do the following:
    
    Verify signature is to call a URL where the keys to the signature are
    called JSON Web Key Set (JWKS). This URL is public, and each Auth0
    tenant has one. The application would use the JWKS to verify that the
    signature in the JWT matches.
    
    Once the signature is verified, there are three standard claims, that
    now need to be validated here is how the claims they are going to be
    validated and how they are validated:
    
     1. exp - Expiration and confirm it hasn't expired
     2. iss - Issued by which then needs to confirm it matches your Auth0
              domain
     3. aud - Audience to make sure it matches your clientID

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const jwt = require('express-jwt') // validate JWT and set req.user
+const jwksRsa = require('jwks-rsa') // Retrieve RSA keys from a JSON Web Key set (JWKS) enpoint
 
 require('dotenv').config()
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header
+  // and the signing keys provided by the JWKs endpoint
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, // cache the signing key
+    rateLimit: true,
+    jwksRequestPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+    jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+  // Validate the audience and the issuer.
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+})
 
 const app = express()
 
