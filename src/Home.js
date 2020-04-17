c817a9bf5e4988485a54f73ca10194d0698698bf
Jason Stathopulos <jstathopulos@gmail.com>
2020-04-17 13:05:28 -0400

    Add `isAuthenticated` to Auth Class
    Add `isAuthenticated` to Auth class so that only when the user is login
    they can display session only content and display content only when the
    user is logout.

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = ({ auth }) => {
   return (
     <section>
       <h1>Home</h1>
 
-      <button onClick={auth.login}>Log In</button>
+      {auth.isAuthenticated() ? (
+        <Link to="/profile">View Profile</Link>
+      ) : (
+        <button onClick={auth.login}>Log In</button>
+      )}
     </section>
   )
 }
